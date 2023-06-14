2a70a4609d2f16ac499d6ed38e6008552ff552c3
Gitari65 <agitari65@gmail.com>
2023-06-14 22:22:20 +0300

    news homepage

---
+++ app/src/main/java/com/example/sautinews/NewsHomePage.java
@@ -0,0 +1,14 @@
+package com.example.sautinews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewsHomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_home_page);
+    }
+}
