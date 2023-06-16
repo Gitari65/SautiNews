c39a940bc02ae9248b17d6d0e7bbc2c9d97174b9
Gitari65 <agitari65@gmail.com>
2023-06-16 18:10:59 +0300

    news homepage

---
+++ app/src/main/java/com/example/sautinews/SigninOptionActivity.java
@@ -0,0 +1,14 @@
+package com.example.sautinews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SigninOptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin_option);
+    }
+}
