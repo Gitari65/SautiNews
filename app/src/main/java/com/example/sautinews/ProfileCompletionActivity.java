24cd21e0a9f129e9df1182322205c383fcd90070
Gitari65 <agitari65@gmail.com>
2023-06-17 21:56:41 +0300

    news homepage

--- app/src/main/java/com/example/sautinews/ProfileCompletionActivity.java
+++ app/src/main/java/com/example/sautinews/ProfileCompletionActivity.java
@@ -2,41 +2,62 @@ package com.example.sautinews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.Calendar;
 
 public class ProfileCompletionActivity extends AppCompatActivity {
-DatePickerDialog datePickerDialog;
-TextView date;
+    DatePickerDialog datePickerDialog;
+    TextView dateTextView;
+    Spinner spinner;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_completion);
-
+        dateTextView = findViewById(R.id.txtView_date);
         final Calendar c = Calendar.getInstance();
+        String[] sex={"choose..","male","female"};
 
-        int mYear = c.get(Calendar.YEAR); // current year
-        int mMonth = c.get(Calendar.MONTH); // current month
-        int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
-        // date picker dialog
-        datePickerDialog = new DatePickerDialog(MainActivity.this,
-                new DatePickerDialog.OnDateSetListener() {
+        // Define a button or any other view that triggers the date picker dialog
 
-                    @Override
-                    public void onDateSet(DatePicker view, int year,
-                                          int monthOfYear, int dayOfMonth) {
-                        // set day of month , month and year value in the edit text
-                        date.setText(dayOfMonth + "/"
-                                + (monthOfYear + 1) + "/" + year);
+        dateTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the current date
+                Calendar calendar = Calendar.getInstance();
+                int mYear = calendar.get(Calendar.YEAR);
+                int mMonth = calendar.get(Calendar.MONTH);
+                int mDay = calendar.get(Calendar.DAY_OF_MONTH);
 
-                    }
-                }, mYear, mMonth, mDay);
-        datePickerDialog.show();
-    }
-});
+                // Create a date picker dialog
+                DatePickerDialog datePickerDialog = new DatePickerDialog(ProfileCompletionActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                // Set the selected date in a TextView or EditText
+
+                                dateTextView.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, mYear, mMonth, mDay);
+
+                // Show the date picker dialog
+                datePickerDialog.show();
+            }
+        });
+
+       spinner=findViewById(R.id.spinner_dob);
+        ArrayAdapter arrayAdapter=new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,sex);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+
+
     }
 }
