ab44c8675fc3052547d7100186a985b18caf6a0a
Gitari65 <agitari65@gmail.com>
2023-06-27 12:02:21 +0300

    news homepage

---
+++ app/src/main/java/com/example/sautinews/EditArticleActivity.java
@@ -0,0 +1,13 @@
+package com.example.sautinews;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_article);
+    }
+}
