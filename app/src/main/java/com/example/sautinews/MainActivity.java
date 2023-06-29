b031621fbbf139e89af6a6d7656de8ff7b007330
Gitari65 <agitari65@gmail.com>
2023-06-29 21:55:18 +0300

    news homepage

--- app/src/main/java/com/example/sautinews/MainActivity.java
+++ app/src/main/java/com/example/sautinews/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.sautinews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
-
+Handler handler;
+ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progressBar=new ProgressBar(MainActivity.this);
+        progressBar.setVisibility(View.VISIBLE);
+        handler=new Handler();
+        handler.postDelayed(()-> {
+
+        },3000);
+        // Check if it's the first time opening the app
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        boolean isFirstTime = sharedPreferences.getBoolean("isFirstTime", true);
+
+        if (isFirstTime) {
+            // First time opening the app, start the FirstTimeActivity
+            Intent intent= new Intent(MainActivity.this,WelcomeMainActivity.class);
+            startActivity(intent);
+
+            // Set the flag indicating that it's not the first time anymore
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("isFirstTime", false);
+            editor.apply();
+        } else {
+            // Not the first time, start another activity
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+
+        // Finish the launcher activity to prevent going back to it
+        finish();
+
+
     }
 }
