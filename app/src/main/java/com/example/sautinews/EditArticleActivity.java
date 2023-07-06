acc89dd9592fb8a150dde5ad90d478799c3e9415
Gitari65 <agitari65@gmail.com>
2023-07-06 23:07:14 +0300

    news homepage

--- app/src/main/java/com/example/sautinews/EditArticleActivity.java
+++ app/src/main/java/com/example/sautinews/EditArticleActivity.java
@@ -1,12 +1,15 @@
 package com.example.sautinews;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.Layout;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.text.style.AlignmentSpan;
 import android.text.style.BulletSpan;
@@ -14,10 +17,12 @@ import android.text.style.RelativeSizeSpan;
 import android.text.style.StyleSpan;
 import android.text.style.UnderlineSpan;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -28,9 +33,12 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.sql.Timestamp;
 import java.util.HashMap;
+import java.util.UUID;
 
 public class EditArticleActivity extends AppCompatActivity {
 
@@ -48,12 +56,22 @@ public class EditArticleActivity extends AppCompatActivity {
     private ImageButton undoButton;
     private ImageButton redoButton;
     private ImageButton spellCheckButton;
-
+    private DatabaseReference myRef;
     private  UndoManager undoManager;
     private TextView textViewdesc;
     private EditText editTextArticleTitle;
     private ImageView imageViewArticlePicture;
     private long timestamp;
+    String imageurl;
+  private Button buttonSave;
+    private TextView txtViewPublish;
+    private Uri selectedImageUri;
+    private FirebaseStorage firebaseStorage;
+    private StorageReference storageReference;
+
+    public EditArticleActivity() {
+    }
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,6 +96,8 @@ public class EditArticleActivity extends AppCompatActivity {
         redoButton = findViewById(R.id.redoButton);
         spellCheckButton = findViewById(R.id.spellCheckButton);
         undoManager = new UndoManager(editText);
+        buttonSave=findViewById(R.id.button_saveArticle);
+       txtViewPublish=findViewById(R.id.button_publishArticle);
         // Set OnClickListener for the boldButton
         boldButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -182,7 +202,31 @@ public class EditArticleActivity extends AppCompatActivity {
                 spellCheck();
             }
         });
+        // Set OnClickListener for the save button
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String status="saved";
+                finishArticle(status);
+            }
+        });
+        // Set OnClickListener for the publish textView
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String status="published";
+                finishArticle(status);
+            }
+        });
+        imageViewArticlePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGallery();
+            }
+        });
     }
+
+
 
     private void toggleBold() {
         // Toggle the selected text between bold and normal
@@ -237,26 +281,38 @@ public class EditArticleActivity extends AppCompatActivity {
             }
         });
     }
-    //Save and piblish method for the Article
+    //Save and publish method for the Article
     public void finishArticle(String Status)
     {
+        if (TextUtils.isEmpty(editTextArticleTitle.getText().toString())) {
+            Toast.makeText(this, "Please state the Title of your Article", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(editText.getText().toString())) {
+            Toast.makeText(this, "Please state the Content of your Article", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        savePicture();
         timestamp=System.currentTimeMillis();
         String title=editTextArticleTitle.getText().toString();
         String Content=editText.getText().toString();
         String currentId=FirebaseAuth.getInstance().getCurrentUser().getUid();
-        DatabaseReference myRef=FirebaseDatabase.getInstance().getReference().child("Article");
+        myRef=FirebaseDatabase.getInstance().getReference().child("Article").child(Status);
         HashMap<Object,Object> artcleMap=new HashMap<>();
         artcleMap.put("ArticleTitle",title);
         artcleMap.put("ArticleContent",Content);
         artcleMap.put("timeStamp",timestamp);
+        artcleMap.put("imageUrl",imageurl);
         myRef.setValue(artcleMap).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
                 if(task.isSuccessful())
                 {
+                    Toast.makeText(EditArticleActivity.this, Status, Toast.LENGTH_SHORT).show();
 
                 }else {
 
+                    Toast.makeText(EditArticleActivity.this, "Failed try again later", Toast.LENGTH_SHORT).show();
                 }
             }
         }).addOnFailureListener(new OnFailureListener() {
@@ -428,5 +484,52 @@ public class EditArticleActivity extends AppCompatActivity {
             undoManager.redo();
         }
     }
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, 1);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == 1) {
+            selectedImageUri = data.getData();
+            imageViewArticlePicture.setImageURI(selectedImageUri);
+        }
+    }
+    private void savePicture() {
+        // Retrieve other article details from the EditText fields (author, topic, content)
+        // ...
+
+        // Get the current timestamp
+
+
+        // Generate a unique filename for the image
+        String imageFileName = "article_cover_" + UUID.randomUUID().toString() + ".jpg";
+
+        // Create a reference to the image file on Firebase Storage
+        StorageReference imageRef = storageReference.child("article_covers/" + imageFileName);
+
+        // Upload the image to Firebase Storage
+        if (selectedImageUri != null) {
+            imageRef.putFile(selectedImageUri)
+                    .addOnSuccessListener(taskSnapshot -> {
+                        // Image upload successful, get the download URL
+                        imageRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                            // Create a new article object with the download URL of the image
+
+                           imageurl=uri.toString();
+
+
+                        });
+                    })
+                    .addOnFailureListener(e -> {
+                        // Image upload failed
+                        // Handle the error appropriately
+                    });
+        } else {
+            // If no image was selected, create an article object without the image URL
+            Toast.makeText(this, "image upload failed", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
