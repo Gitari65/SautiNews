d4c487fc5b140e6b6c9007646e3529d29736ea7f
Gitari65 <agitari65@gmail.com>
2023-07-18 18:35:10 +0300

    news homepage

--- app/src/main/java/com/example/sautinews/HomeFragment.java
+++ app/src/main/java/com/example/sautinews/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.sautinews;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
@@ -23,6 +25,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Queue;
 
@@ -73,41 +76,72 @@ public class HomeFragment extends Fragment {
         }
     }
     private RecyclerView recyclerView,recyclerViewRecent,recyclerViewSuggestion;
-    private AdapterArticle articleAdapter;
-    private List<Article> articles;
-    private DatabaseReference articlesRef;
+    private AdapterArticle articleAdapter,articleAdapterRecent;
+    private List<Article> articles,articlesRecent;
+    private DatabaseReference articlesRef,articlesRefRecent;
+    ProgressBar progressBar;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
        View view= inflater.inflate(R.layout.fragment_home, container, false);
-
+       progressBar=view.findViewById(R.id.progressBarHomeFragemnt);
         recyclerView = view.findViewById(R.id.MyArticlerecyclerView);
         recyclerViewRecent=view.findViewById(R.id.RecentArticleRecyclerView);
         recyclerViewSuggestion = view.findViewById(R.id.SuggestionrecyclerView);
+        checkProfileComplete();
         // Set up LinearLayoutManager with horizontal orientation
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
-        recyclerView.setLayoutManager(layoutManager);
+        // Set up LinearLayoutManager with horizontal orientation for recent articles RecyclerView
+        LinearLayoutManager layoutManagerRecent = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewRecent.setLayoutManager(layoutManagerRecent);
 
+        // Set up LinearLayoutManager with horizontal orientation for my articles RecyclerView
+        LinearLayoutManager layoutManagerMyArticles = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(layoutManagerMyArticles);
         getMyArticleData();
+        getRecentArticleData();
 
        return view;
+    }
+    public void checkProfileComplete()
+    {
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid().toString();
+
+        DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference().child("users");
+
+        Query usernameQuery = usersRef.orderByChild("authorId").equalTo(userId);
+
+        usernameQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (!dataSnapshot.exists()) {
+                    // Username already exists
+                    // Handle the case accordingly
+                    Intent intent=new Intent(getContext(),ProfileCompletionActivity.class);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // An error occurred while retrieving the data
+                // Handle the error accordingly
+            }
+        });
+
     }
     public void getMyArticleData() {
         // Initialize the list to hold Article objects
         articles = new ArrayList<>();
 
         // Get the current user's ID
-        String currentUserId = FirebaseAuth.getInstance().getCurrentUser().getUid().toString(); // Replace this with your method to retrieve the current user's ID
+        String currentUserId =FirebaseAuth.getInstance().getCurrentUser().getUid().toString(); // Replace this with your method to retrieve the current user's ID
 
         // Get a reference to the "articles" node in the Firebase Realtime Database
         articlesRef = FirebaseDatabase.getInstance().getReference("Articles").child("published");
 
-        // Query the articlesRef to filter by authorId and order the data by the "timestamp" child node in descending order
-        Query query = articlesRef.orderByChild("authorId").equalTo(currentUserId).orderByChild("timestamp");
-
         // Set up a ValueEventListener to listen for changes in the data
-        query.addValueEventListener(new ValueEventListener() {
+        articlesRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 // Clear the existing list of articles
@@ -117,11 +151,20 @@ public class HomeFragment extends Fragment {
                 for (DataSnapshot articleSnapshot : snapshot.getChildren()) {
                     // Get the article data and create an Article object
                     Article article = articleSnapshot.getValue(Article.class);
-                    articles.add(article);
+
+                    // Filter the articles by authorId
+                    if (article != null && article.getAuthorId().equals(currentUserId)) {
+                        articles.add(article);
+                    }
                 }
 
-                // Reverse the list to display the items from most recent to oldest
-                Collections.reverse(articles);
+                // Sort the list by timestamp in descending order
+                Collections.sort(articles, new Comparator<Article>() {
+                    @Override
+                    public int compare(Article article1, Article article2) {
+                        return article2.getTimestamp().compareTo(article1.getTimestamp());
+                    }
+                });
 
                 // Create an instance of the ArticleAdapter and pass the list of articles
                 articleAdapter = new AdapterArticle(articles, getActivity());
@@ -137,38 +180,39 @@ public class HomeFragment extends Fragment {
         });
     }
 
+
     public void getRecentArticleData() {
         // Initialize the list to hold Article objects
-        articles = new ArrayList<>();
+        articlesRecent = new ArrayList<>();
 
         // Get a reference to the "articles" node in the Firebase Realtime Database
-        articlesRef = FirebaseDatabase.getInstance().getReference("Articles").child("published");
+        articlesRefRecent = FirebaseDatabase.getInstance().getReference("Articles").child("published");
 
         // Query the articlesRef to order the data by the "timestamp" child node in descending order
-        Query query = articlesRef.orderByChild("timestamp");
+        Query query = articlesRefRecent.orderByChild("timestamp");
 
         // Set up a ValueEventListener to listen for changes in the data
         query.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 // Clear the existing list of articles
-                articles.clear();
+                articlesRecent.clear();
 
                 // Iterate through the dataSnapshot to retrieve Article objects
                 for (DataSnapshot articleSnapshot : snapshot.getChildren()) {
                     // Get the article data and create an Article object
                     Article article = articleSnapshot.getValue(Article.class);
-                    articles.add(article);
+                    articlesRecent.add(article);
                 }
 
                 // Reverse the list to display the items from most recent to oldest
-                Collections.reverse(articles);
+                Collections.reverse(articlesRecent);
 
                 // Create an instance of the ArticleAdapter and pass the list of articles
-                articleAdapter = new AdapterArticle(articles, getActivity());
+                articleAdapterRecent = new AdapterArticle(articlesRecent, getActivity());
 
                 // Set the adapter on the RecyclerView
-                recyclerViewRecent.setAdapter(articleAdapter);
+                recyclerViewRecent.setAdapter(articleAdapterRecent);
             }
 
             @Override
@@ -180,7 +224,7 @@ public class HomeFragment extends Fragment {
     public void getSuggestionArticleData() {
         // Initialize the list to hold Article objects
         articles = new ArrayList<>();
-
+progressBar.setVisibility(View.VISIBLE);
         // Get a reference to the "articles" node in the Firebase Realtime Database
         articlesRef = FirebaseDatabase.getInstance().getReference("Suggestions").child("published");
 
@@ -209,11 +253,12 @@ public class HomeFragment extends Fragment {
 
                 // Set the adapter on the RecyclerView
                 recyclerView.setAdapter(articleAdapter);
+                progressBar.setVisibility(View.GONE);
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
+                progressBar.setVisibility(View.GONE);
             }
         });
     }
