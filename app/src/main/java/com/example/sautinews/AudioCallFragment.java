2db5aac57b50fc535d1778626dd75c97a6139df8
Gitari65 <agitari65@gmail.com>
2023-08-29 20:09:36 +0300

    audio call FEATURES

--- app/src/main/java/com/example/sautinews/AudioCallFragment.java
+++ app/src/main/java/com/example/sautinews/AudioCallFragment.java
@@ -1,12 +1,31 @@
 package com.example.sautinews;
 
+import static io.agora.base.internal.ContextUtils.getApplicationContext;
+
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+//import voice sdks
+import io.agora.rtc2.Constants;
+import io.agora.rtc2.IRtcEngineEventHandler;
+import io.agora.rtc2.RtcEngine;
+import io.agora.rtc2.RtcEngineConfig;
+import io.agora.rtc2.ChannelMediaOptions;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -55,10 +74,137 @@ public class AudioCallFragment extends Fragment {
         }
     }
 
+//audio permissions
+private static final int PERMISSION_REQ_ID = 22;
+    private static final String[] REQUESTED_PERMISSIONS =
+            {
+                    Manifest.permission.RECORD_AUDIO
+            };
+
+    private boolean checkSelfPermission()
+    {
+        if (ContextCompat.checkSelfPermission(getActivity(), REQUESTED_PERMISSIONS[0]) !=  PackageManager.PERMISSION_GRANTED)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    void showMessage(String message) {
+        getActivity().runOnUiThread(() ->
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show());
+    }
+
+
+//variables
+// Fill the App ID of your project generated on Agora Console.
+private final String appId = "34ee3cb8a7d84da4ad7a77edf6ee1d90";
+    // Fill the channel name.
+    private String channelName = "Spark Verse";
+    // Fill the temp token generated on Agora Console.
+    private String token = "<your access token>";
+    // An integer that identifies the local user.
+    private int uid = 0;
+    // Track the status of your connection
+    private boolean isJoined = false;
+
+    // Agora engine instance
+    private RtcEngine agoraEngine;
+    // UI elements
+    private TextView infoText;
+    private Button joinLeaveButton;
+    //private IRtcEngineEventHandler mRtcEventHandler = new MyRtcEventHandler();
+    private void setupVoiceSDKEngine() {
+        try {
+            RtcEngineConfig config = new RtcEngineConfig();
+            config.mContext = requireContext();
+            config.mAppId = appId;
+            config.mEventHandler = mRtcEventHandler;
+            agoraEngine = RtcEngine.create(config);
+        } catch (Exception e) {
+            throw new RuntimeException("Check the error.");
+        }
+    }
+    private final IRtcEngineEventHandler mRtcEventHandler = new IRtcEngineEventHandler() {
+        @Override
+        // Listen for the remote user joining the channel.
+        public void onUserJoined(int uid, int elapsed) {
+            getActivity().runOnUiThread(()->infoText.setText("Remote user joined: " + uid));
+        }
+
+        @Override
+        public void onJoinChannelSuccess(String channel, int uid, int elapsed) {
+            // Successfully joined a channel
+            isJoined = true;
+            showMessage("Joined Channel " + channel);
+            getActivity().runOnUiThread(()->infoText.setText("Waiting for a remote user to join"));
+        }
+
+        @Override
+        public void onUserOffline(int uid, int reason) {
+            // Listen for remote users leaving the channel
+            showMessage("Remote user offline " + uid + " " + reason);
+            if (isJoined) getActivity().runOnUiThread(()->infoText.setText("Waiting for a remote user to join"));
+        }
+
+        @Override
+        public void onLeaveChannel(RtcStats 	stats) {
+            // Listen for the local user leaving the channel
+            getActivity().runOnUiThread(()->infoText.setText("Press the button to join a channel"));
+            isJoined = false;
+        }
+    };
+
+
+    private void joinChannel() {
+        ChannelMediaOptions options = new ChannelMediaOptions();
+        options.autoSubscribeAudio = true;
+        // Set both clients as the BROADCASTER.
+        options.clientRoleType = Constants.CLIENT_ROLE_BROADCASTER;
+        // Set the channel profile as BROADCASTING.
+        options.channelProfile = Constants.CHANNEL_PROFILE_LIVE_BROADCASTING;
+
+        // Join the channel with a temp token.
+        // You need to specify the user ID yourself, and ensure that it is unique in the channel.
+        agoraEngine.joinChannel(token, channelName, uid, options);
+    }
+
+    public void joinLeaveChannel(View view) {
+        if (isJoined) {
+            agoraEngine.leaveChannel();
+            joinLeaveButton.setText("Join");
+        } else {
+            joinChannel();
+            joinLeaveButton.setText("Leave");
+        }
+    }
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_audio_call, container, false);
+       View view= inflater.inflate(R.layout.fragment_audio_call, container, false);
+        if (!checkSelfPermission()) {
+            ActivityCompat.requestPermissions(getActivity(), REQUESTED_PERMISSIONS, PERMISSION_REQ_ID);
+        }
+
+        setupVoiceSDKEngine();
+
+        // Set up access to the UI elements
+        joinLeaveButton = view.findViewById(R.id.joinLeaveButton);
+        infoText = view.findViewById(R.id.infoText);
+       return view;
     }
+    public void onDestroy() {
+        super.onDestroy();
+        agoraEngine.leaveChannel();
+
+        // Destroy the engine in a sub-thread to avoid congestion
+        new Thread(() -> {
+            RtcEngine.destroy();
+            agoraEngine = null;
+        }).start();
+    }
+
 }
